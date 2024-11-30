ca77d86e5ec710ff9ddd96399a9b451d33925cf2
Camilo Medina <medina1camilo@gmail.com>
2024-11-30 19:18:47 +0100

    develop
    fix Dockerfile, compose.yaml and add schema.sql, data.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE products
+(
+    sku         VARCHAR(10) PRIMARY KEY,
+    price       DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255)   NOT NULL,
+    category    VARCHAR(100)   NOT NULL
+);
